cae80a0eb6a60efb6e2a116a25ab68e2dc3e0de5
Eric York <eric@calthings.com>
2020-05-07 05:41:40 +0000

    Test

--- Sources/COpenSSL/openssl.h
+++ Sources/COpenSSL/openssl.h
@@ -111,7 +111,31 @@ struct evp_cipher_st {};
 struct evp_cipher_ctx_st {};
 struct evp_md_st {};
 struct evp_md_ctx_st {};
-struct evp_pkey_st {};
+// struct evp_pkey_st {};
+struct evp_pkey_st {
+	    int type;
+	        int save_type;
+		    int references;
+		        const EVP_PKEY_ASN1_METHOD *ameth;
+			    ENGINE *engine;
+			        union {
+					        char *ptr;
+# ifndef OPENSSL_NO_RSA
+						        struct rsa_st *rsa;     /* RSA */
+# endif
+# ifndef OPENSSL_NO_DSA
+							        struct dsa_st *dsa;     /* DSA */
+# endif
+# ifndef OPENSSL_NO_DH
+								        struct dh_st *dh;       /* DH */
+# endif
+# ifndef OPENSSL_NO_EC
+									        struct ec_key_st *ec;   /* ECC */
+# endif
+										    } pkey;
+				    int save_parameters;
+				        STACK_OF(X509_ATTRIBUTE) *attributes; /* [ 0 ] */
+} /* EVP_PKEY */ ;
 struct evp_pkey_asn1_method_st {};
 struct evp_pkey_method_st {};
 struct evp_pkey_ctx_st {};
